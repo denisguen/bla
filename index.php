d5a0ee95edbb7695e017e95b10535548d4d3628f
denisguen <denis@dgcgn.com>
2021-01-12 17:40:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-<script>
-  const input = document.querySelector('input');
-  const log = document.getElementById('values');
-  input.addEventListener('input', updateValue);
+ <head>
+  <script>
+    const input = document.querySelector('input');
+    const log = document.getElementById('values');
+    input.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
-</script>
-</head>
-<body>
-
-  <table>
-    <tr id="row0">
-      <td id="00">
-        <input type="text" id="s00">
-      </td>
-      <td id="01">
-        <input type="text" id="s01">
-      </td>
-      <td id="02">
-        <input type="text" id="s02">
-      </td>
-    </tr>
-  </table>
-  <br />
-
-  <input type="button" onclick="changeText()" value="Set text of selected option">
+    function updateValue(e) {
+    log.textContent = e.target.value;
+    }
+  </script>
+ </head>
+ <body>
+  <input placeholder="Enter some text" name="name"/>
   <p id="values"></p>
-</body>
+ </body>
 </html>
