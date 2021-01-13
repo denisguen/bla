0f49936a6489ad8b10e47e5125a2c2fbea810b0e
denisguen <denis@dgcgn.com>
2021-01-13 17:54:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  
+
  </head>
  <body>
   <label>Choose an ice cream flavor:
@@ -12,15 +12,33 @@
       <option value="vanilla">Vanilla</option>
     </select>
   </label>
+  <input type="text" id="t1" />
+  <input type="text" id="t2" />
+  <input type="text" id="t3" />
 
   <div class="result"></div>
   <script>
    const selectElement = document.querySelector('.ice-cream');
+   const allInputs = document.querySelector('input');
+
    selectElement.addEventListener("change", setVal);
+
+   forEach((singleInput in allInputs) => {
+     singleInput.addEventListener("change", setVal);
+   });
+
    function setVal(sender){
+     switch (sender.type) {
+       case select:
+         alert("Select");
+         break;
+       default:
+         alert("was anderes");
+     }
      const result = document.querySelector('.result');
      result.textContent = `You like ${sender.target.value}`;
    }
+
   </script>
  </body>
 </html>
